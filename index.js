f001816890c0fb9fa5ab629806778c81c4131a76
evelynym <evelynym@outlook.com>
2022-03-27 19:18:16 +0800

    put connection string  into dotenv

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import experimentRouter from "./routes/experiments.js";
 import answerRouter from "./routes/answers.js";
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,11 +16,12 @@ app.use(cors());
 app.use("/experiments", experimentRouter);
 app.use("/answers", answerRouter);
 
-const CONNECTION_URL =
-  "mongodb+srv://doorstepsmastery:doorstepsmastery123@cluster0.sy8bw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(PORT, () => console.log(`server running`)))
   .catch((e) => console.log(e.message));
